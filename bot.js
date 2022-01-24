2fc0e3e06eb03030b5577b61b807f1d6383e4c18
Glitch (awesome-silly-piper) <none>
2022-01-24 19:10:16 +0000

    🖐️😛 Updated with Glitch

---
+++ bot.js
@@ -0,0 +1,16 @@
+const discordBotkit = require("botkit-discord");
+
+const configuration = {
+  token: process.env.DISCORD_TOKEN,
+};
+
+// const discordBot = discordBotkit(configuration);
+
+// var normalizedPath = require("path").join(__dirname, "skills");
+// require("fs")
+//   .readdirSync(normalizedPath)
+//   .forEach(function (file) {
+//     require("./skills/" + file)(discordBot);
+//   });
+
+// module.exports = discordBot;

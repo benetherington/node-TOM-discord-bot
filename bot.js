d611bf08d6462ce42579da46037a39caae6136fe
benetherington <ben.j.etherington@gmail.com>
2022-02-02 16:45:53 -0500

    Continue writing receiveButton logic

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ for (const fileName of interactionFileNames) {
     let slash = require("./slash/"+fileName);
     client.slashes.set(slash.data.name, slash)
 }
-const {recieveButton} = require("./buttons/buttons.js");
+const {receiveButton} = require("./buttons/buttons.js");
 
 /*
 EVENTS
@@ -48,7 +48,7 @@ client.on("interactionCreate", async interaction=>{
             interaction.reply(responses.failure)
         }
     } else if (interaction.isButton()) {
-        recieveButton(interaction)
+        receiveButton(interaction)
     }
 })
 
--- buttons/buttons.js
+++ buttons/buttons.js
@@ -1,8 +1,20 @@
-const {addVoter} = require("./src/sqlite.js");
+const {countVotesOnSuggestion, addVoterToSuggestion, getSuggestion} = require("../src/sqlite.js");
+const {formatTitleReply} = require("../slash/title.js")
 
-const recieveButton = interaction=>{
-    const token = interaction.customId;
-    const voteTotal = addVoter({messageId:token});
+const receiveButton = async interaction=>{
+    const voter = {
+        discordId: interaction.user.id,
+    };
+    const suggestion = {
+        suggestionId: interaction.customId
+    }
     
+    await addVoterToSuggestion(voter, suggestion);
+    const voteCount = await countVotesOnSuggestion(suggestion);
+    const {text} = await getSuggestion(suggestion);
+    suggestion.text = text
+    
+    interaction.update(formatTitleReply(voter, suggestion, voteCount))
 };
 
+module.exports = {receiveButton}

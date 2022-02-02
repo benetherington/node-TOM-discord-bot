8fee15e849cd981bec19586bd409045bd251a0dc
benetherington <ben.j.etherington@gmail.com>
2022-02-02 18:04:54 -0500

    Tidy db initialization. Remove re-re-re-awaited Promise.

--- src/sqlite.js
+++ src/sqlite.js
@@ -1,17 +1,5 @@
-/**
- * Module handles database management
- *
- * Server API calls the methods in here to query and update the SQLite database
- */
-
-
-/*--------*\
-  DB SETUP
-\*--------*/
 const dbFile = require('path').resolve("./.data/title-suggestions.db");
 const sqlite3 = require("sqlite3").verbose();
-// We're using the sqlite wrapper so that we can make async / await connections
-// - https://www.npmjs.com/package/sqlite
 const dbWrapper = require("sqlite");
 let db;
 
@@ -20,104 +8,38 @@ let db;
   UTILITIES
 \*---------*/
 // await assureLoaded before interacting with the database
-let assureLoaded = async()=>{await wrapperPromise;}
-let printDbSummary = async ()=>{
-        try {
-                // TODO: running this SELECT query works from the command line tool, but
-                // generates a "no table" error inside node.
-                let selectTables = await db.all("SELECT name FROM sqlite_master WHERE type='table'");
-                let exists = selectTables.map(row=>row.name).join(", ");
-                console.log(`Waking up SQLite. Tables: ${exists}`)
+const printDbSummary = async ()=>{
+    try {
+        const selectTables = await db.all("SELECT name FROM sqlite_master WHERE type='table'");
+        const exists = selectTables.map(row=>row.name).join(", ");
+        console.log(`Waking up SQLite. Tables: ${exists}`)
 
-                let selectSuggestions = await db.all("SELECT text from Suggestions ORDER BY suggestionId LIMIT 10");
-                if (selectSuggestions.length) {
-                    firstSuggestions = selectSuggestions.map(r=>r.suggestion).join(", ");
-                    console.log(`Most recent suggestions: ${firstSuggestions}`)
-                } else {
-                    console.log("No suggestions have been made yet.")
-                }
-        } catch (error) {
-                console.error("There was an issue printing the db summary.")
-                console.error(error)
+        const suggestions = await db.all("SELECT text from Suggestions ORDER BY suggestionId LIMIT 10");
+        if (suggestions.length) {
+            const suggestionsString = suggestions.map(r=>r.suggestion).join(", ");
+            console.log(`Most recent suggestions: ${suggestionsString}`)
+        } else {
+            console.log("No suggestions have been made yet.")
         }
+    } catch (error) {
+            console.error("There was an issue printing the db summary.")
+            console.error(error)
+    }
 }
 
 
 /*-------*\
   DB INIT
 \*-------*/
-let wrapperPromise = dbWrapper
-    .open({
+const initDB = async ()=>{
+    db = await dbWrapper.open({
         filename: dbFile,
         driver: sqlite3.cached.Database
-    })
-    .then(async dBase => {
-        db = dBase;
-        try {
-            await db.migrate() // defaults to no force, table migrations, path ./migrations
-            await printDbSummary()
-        } catch (dbError) {
-            console.error(dbError);
-        }
-    })
-    .catch(e=>console.error(e))
-
-
-
-/*------------------*\
-  DB REPRESENTATIONS
-\*-----------------*/
-// class Permittable extends Object {
-//     constructor() {
-//         const permittable = super();
-//         Object.defineProperties(permittable, {
-//             recordId: {enumerable: true}
-//         })
-//     }
-    
-//     /*
-//     Property input and output
-//     */
-//     permit(options) {
-//         const propertyNames = Object.getOwnPropertyNames(this);
-//         for (const propertyName of propertyNames) {
-//             if (options[propertyName]) {
-//                 this[propertyName] = options[propertyName];
-//             }
-//         }
-//     }
-//     simplify() {
-//         return new Object.fromEntries(Object.entries(this))
-//     }
-// }
-
-
-// class PermittedAuthor extends Permittable {
-//     constructor(options={}) {
-//         let author = super();
-//         Object.defineProperties(author, {
-//             id: {enumerable: true},
-//             username: {enumerable: true},
-//             displayName: {enumerable: true},
-//         })
-//         author.permit(options)
-//     }
-// }
-
-// class PermittedSuggestion extends Permittable {
-//     constructor(options={}) {
-//         let suggestion = super();
-//         Object.defineProperties(suggestion, {
-//             authorId: {enumerable:true},
-//             episodeId: {enumerable:true},
-//             token: {enumerable: true},
-//             text: {enumerable:true},
-//             voters: {enumerable: true}
-//         })
-//         suggestion.permit(options);
-//     }
-// }
-
+    });
+    await db.migrate()
+    printDbSummary()
+}
+initDB();
 
 /*-------*\
   EXPORTS

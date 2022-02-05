32d242d848815e29123cf87bfc7529109e8a3ba2
benetherington <ben.j.etherington@gmail.com>
2022-02-05 13:35:49 -0500

    Switch to 5 rows * 5 buttons.

--- slash/vote.js
+++ slash/vote.js
@@ -8,13 +8,6 @@ let data = new SlashCommandBuilder()
     .toJSON()
 ;
 
-const buttonColors = [
-    {style:"PRIMARY",   color:"#5865f2"},
-    {style:"SECONDARY", color:"#4f545c"},
-    {style:"SUCCESS",   color:"#3ba55c"},
-    {style:"DANGER",    color:"#ed4245"}
-]
-
 const chunkArray = (toChunk, chunkSize)=>{
     let chunked = [];
     while (toChunk.length) {
@@ -22,57 +15,29 @@ const chunkArray = (toChunk, chunkSize)=>{
     }
     return chunked;
 }
-
-
-
-
-const formatVoteButton = (style, {author, suggestion, voteCount=1})=>{
-    // Returns a formatted button
+const formatVoteButton = ({author, suggestion, voteCount=1})=>{
+    // Turns a countedSuggestion into a button.
     return new MessageButton()
-        .setLabel(`VOTE`)
-        .setStyle(style)
+        .setLabel(`(${voteCount}) ${author.displayName||author.username}: ${suggestion.text}`)
+        .setStyle("SECONDARY")
         .setCustomId(suggestion.suggestionId.toString())
-}
-const formatVoteButtons = (countedSugestionChunk)=>{
-    // Returns a MessageActionRow full of buttons
-    const buttons = countedSugestionChunk.map((countedSuggestion, idx)=>{
-        const style = buttonColors[idx].style;
-        return formatVoteButton(style, countedSuggestion)
-    })
-    return [new MessageActionRow().setComponents(buttons)]
-}
-
-const formatVoteEmbed = (color, {author, suggestion, voteCount=1})=>{
-    // Returns a formatted embed
-    return new MessageEmbed()
-        .setColor(color)
-        .setTitle(author.displayName || author.username)
-        .setDescription(suggestion.text);
-}
-const formatVoteEmbeds = (countedSuggestionChunk)=>{
-    return countedSuggestionChunk.map((countedSuggestion, idx)=>{
-        const color = buttonColors[idx].color;
-        return formatVoteEmbed(color, countedSuggestion)
-    })
-}
-
-const formatVoteReply = (countedSuggestionChunk)=>{
-    // Takes (up to five) countedSuggestions, turns them into MessageActionRows
-    // with a button in each. Returns an InteractionReplyOptions Object.
-    const components = formatVoteButtons(countedSuggestionChunk);
-    const embeds = formatVoteEmbeds(countedSuggestionChunk);
-    return {content:"VOTE:", components, embeds}
-}
-const formatVoteReplies = (allCountedSuggestions)=>{
-    // Messages can only contain five MessageActionRows.
-    // Break [allCountedSuggestions] into arrays of 5 suggestions each, then turn
-    // each chunk into a reply with five rows of one button each.
-    const countedSuggestionChunks = chunkArray(allCountedSuggestions, 4);
-    return countedSuggestionChunks.map(formatVoteReply)
-}
-
-
-
+};
+const formatVoteRow = (countedSuggestionRowChunk)=>{
+    // Turns an array of countedSuggestions into a MessageActionRow
+    const buttons = countedSuggestionRowChunk.map(formatVoteButton)
+    return new MessageActionRow().setComponents(buttons)
+};
+const formatVoteMessage = (countedSuggestionMessageChunk)=>{
+    // Turns an array of countedSuggestions into an InteractionReplyOption.
+    const countedSuggestionRowChunks = chunkArray(countedSuggestionMessageChunk, 5)
+    const components = countedSuggestionRowChunks.map(formatVoteRow);
+    return {content:"Title suggestions so far:", components}
+};
+const getVoteReplies = (allCountedSuggestions)=>{
+    // Turns an array of countedSuggestions into an array of InteractionReplyOptions.
+    const countedSuggestionMessageChunks = chunkArray(allCountedSuggestions, 5*5);
+    return countedSuggestionMessageChunks.map(formatVoteMessage)
+};
 
 const execute = async (interaction)=>{
     console.log("New vote request from "+interaction.user.username)
@@ -82,17 +47,18 @@ const execute = async (interaction)=>{
     console.log(`Found ${allCountedSuggestions.length} suggestions to vote on`)
     
     // BUILD response/s
-    const responses = formatVoteReplies(allCountedSuggestions);
+    const responses = getVoteReplies(allCountedSuggestions);
     // TODO:Delete the most recent vote message.
     
-    // SEND responses
     try {
-        await interaction.reply(responses.pop())
+        // SEND first reply
+        await interaction.reply(responses.shift())
+        // SEND follow ups
+        while (responses.length) {
+            await interaction.followUp(responses.shift())
+        }
     } catch (error) {
         console.log(error)
-    }
-    while (responses.length) {
-        await interaction.followUp(responses.pop())
     }
 };
 

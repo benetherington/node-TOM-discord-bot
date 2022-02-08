ab09544365e47366a83df292a95bf3f43174c2b8
benetherington <ben.j.etherington@gmail.com>
2022-02-08 14:59:37 -0500

    add basic webserver

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const path = require("path")
+const {getSuggestionsWithCountedVotes} = require("./sqlite.js");
+
+
+
+async function doRoutes (fastify, app) {
+    
+    fastify.register(require("point-of-view"),{
+        engine: {pug: require("pug")}
+    })
+    
+    
+    fastify.get("/", async (req, res)=>{
+        res.view("src/views/monitor")
+    })
+    
+    fastify.get("/api/titles/:epNum", async (request, reply)=>{
+        const epNum = request.params.epNum;
+        const countedSuggestions = await getSuggestionsWithCountedVotes({epNum});
+        if (!countedSuggestions) throw new Error("invalid");
+        return countedSuggestions;
+    })
+}
+
+module.exports = {doRoutes}

351565195ab9de69ca1315d7cbdb040d214da38b
benetherington <ben.j.etherington@gmail.com>
2022-02-09 18:01:12 -0500

    add static routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,11 @@ const {getSuggestionsWithCountedVotes} = require("./sqlite.js");
 
 
 async function doRoutes (fastify, app) {
+    fastify.register(require('fastify-static'), {
+        root: path.join(__dirname, 'views/icons'),
+        prefix: '/icons/',
+        wildcard: true
+    })
     
     fastify.register(require("point-of-view"),{
         engine: {pug: require("pug")}

d40dfb9972ebfb3582cf448e6e710a59128bf3ee
benetherington <ben.j.etherington@gmail.com>
2022-02-10 22:22:27 -0500

    Update interface function name

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,38 @@
 const path = require("path")
 const {getSuggestionsWithCountedVotes} = require("./sqlite.js");
-const {startNewVote} = require("../interface/bot-monitor-interface.js");
+const {startNewVote} = require("../interface/vote-interface.js");
+const {addNewSuggestion} = require("../interface/title-interface.js");
 
 
 async function doRoutes (fastify, app) {
+    // RENDERING
+    fastify.register(require("point-of-view"),{
+        engine: {pug: require("pug")}
+    })
+    
+    // STATIC
     fastify.register(require('fastify-static'), {
         root: path.join(__dirname, 'views/icons'),
         prefix: '/icons/',
         wildcard: true
     })
     
-    fastify.register(require("point-of-view"),{
-        engine: {pug: require("pug")}
-    })
-    
-    
+    // DYNAMIC
     fastify.get("/", async (req, res)=>{
         res.view("src/views/monitor")
     })
-    
     fastify.get("/api/titles/:epNum", async (request, reply)=>{
         const epNum = request.params.epNum;
         const countedSuggestions = await getSuggestionsWithCountedVotes({epNum});
         if (!countedSuggestions) throw new Error("invalid");
         return countedSuggestions;
     })
+    fastify.post("/api/titles/:messageId", (request, reply)=>{
+        const messageId = request.params.messageId;
+        addNewSuggestionFromApi(messageId)
+    })
     fastify.post("/api/vote", (request, reply)=>{
-        startNewVote();
+        startNewVoteFromApi();
     })
 }
 

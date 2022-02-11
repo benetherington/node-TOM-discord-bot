ff35ba8a27534cb6758481cf3b950633f89a16fc
benetherington <ben.j.etherington@gmail.com>
2022-02-10 22:21:33 -0500

    rename vote interface to be more specific

--- interface/vote-interface.js
+++ interface/vote-interface.js
@@ -3,12 +3,11 @@ const {getVoteMessages} = require("../slash/utilities/vote-utilities.js");
 const ID = require("../src/id.json");
 
 
-const startNewVote = async ()=>{
+const startNewVoteFromApi = async ()=>{
     console.log("Bot monitor GUI requested vote message")
     const voteMessages = await getVoteMessages();
-    const groundControl = await client.channels.fetch(ID.channel.botTest)
     const firstMessage = groundControl.send(voteMessages.shift())
     while (voteMessages.length) await firstMessage.reply(voteMessages.shift())
 };
 
-module.exports = {startNewVote};
+module.exports = {startNewVoteFromApi};

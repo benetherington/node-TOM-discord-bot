3d2eff2c51df295d10ae6bd2829cd7977b06974c
benetherington <ben.j.etherington@gmail.com>
2022-03-21 21:38:16 -0400

    Styling

--- src/views/login.js
+++ src/views/login.js
@@ -1,5 +1,11 @@
 const params = new URLSearchParams(document.location.search);
 if (params.has("auth")) {
     const message = document.getElementById("message");
-    message.innerText = params.get("auth");
+    const status = params.get("auth");
+    
+    if (status==="none"){
+        message.innerText = "You must log in to do that."
+    } else if (status==="failed") {
+        message.innerText = "Wrong username or password."
+    }
 }

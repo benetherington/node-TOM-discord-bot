4b4607c1ef0d5d9d0ed8f94c7c9772b060a6962e
benetherington <ben.j.etherington@gmail.com>
2022-04-29 11:48:37 -0400

    Update cookie, static, and formbody to core modules

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (fastify, opts, done) => {
     // STATIC ROUTES
-    fastify.register(require('fastify-static'), {
+    fastify.register(require('@fastify/static'), {
         root: path.join(__dirname, '../views/icons'),
         prefix: '/icons/',
         wildcard: true,

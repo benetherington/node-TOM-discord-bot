f2352419604ecfc63432ec90ef663a95bd63d31b
benetherington <ben.j.etherington@gmail.com>
2022-05-13 13:32:35 -0400

    Defer /vote reply, just in case it takes longer than 3 seconds (ie it's being debugged.)

--- title-suggestions/slash/vote.js
+++ title-suggestions/slash/vote.js
@@ -8,24 +8,23 @@ const data = new SlashCommandBuilder()
     .toJSON();
 const execute = async (interaction) => {
     console.log(`${interaction.user.username} used /vote`);
+    await interaction.deferReply();
 
     try {
         const responses = await getVoteMessages();
         if (responses.length === 0) {
             // No suggestions yet
-            interaction.reply({
-                content: 'No title suggestions yet!',
-                ephemeral: true,
-            });
+            interaction.editReply(config.nothingToVoteOn);
         } else {
             // TODO:Delete the most recent vote message.
             // SEND reply and followups
-            await interaction.reply(responses.shift());
+            await interaction.editReply(responses.shift());
             while (responses.length) {
                 await interaction.followUp(responses.shift());
             }
         }
     } catch (error) {
+        interaction.editReply(config.failure)
         console.log(error);
     }
 };

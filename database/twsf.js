f10bb11dada9e04a76b7917115f92e164a6d1eb2
benetherington <ben.j.etherington@gmail.com>
2022-05-16 13:26:40 -0400

    Adjunct data to twsf slash handler

--- database/twsf.js
+++ database/twsf.js
@@ -1,4 +1,11 @@
 module.exports.addNewTwsfGuess = ({author, guess}) => {
+    console.log('add new twsf guess');
     console.table({author, guess});
-    return true;
+    return guess;
+};
+
+module.exports.updateTwsfGuess = (guess, messageId) => {
+    console.log('update twsf guess');
+    console.table({guess, messageId});
+    return guess;
 };

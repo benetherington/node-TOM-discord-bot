a03d0e8b325749241401d7d6a12e07f3428c343f
Ben Etherington <ben.j.etherington@gmail.com>
2022-07-16 11:50:10 -0400

    Add file logger

--- logger.js
+++ logger.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const pino = require('pino');
 const pretty = require('pino-pretty');
-const stream = pretty({
-    colorize: true,
-});
-const logger = pino({level: 'info'}, stream);
 
-module.exports = logger;
+// Log to console in development, log to file in production
+let destination;
+if (process.env.NODE_ENV === 'development') {
+    destination = pretty({
+        colorize: true,
+    });
+} else {
+    destination = pino.destination(`${process.cwd()}\\app.log`)
+}
+
+module.exports = pino({level: 'info'}, destination);

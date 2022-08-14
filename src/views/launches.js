8210937f8b637fe6c5fa915cc756d3a7a2d18d00
Glitch (tom-title-suggestion-monitor) <none>
2022-08-14 12:29:39 +0000

    🗾🍘 Checkpoint ./src/views/launches.js:41208475/27

--- src/views/launches.js
+++ src/views/launches.js
@@ -26,6 +26,7 @@ const getNextPage = async (next) => {
 const dateOptions = {
     dateStyle: 'full',
     timeStyle: 'long',
+    hour12: false,
     timeZone: 'utc',
 };
 const buildLaunchRow = (launchData) => {

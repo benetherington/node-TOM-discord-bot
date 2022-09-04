abd9c23ade0a8c6a7bc14d6f60c26e4246277d10
Glitch (tom-title-suggestion-monitor) <none>
2022-09-04 15:24:06 +0000

    🐵🍯 Checkpoint ./twsf/email.js:41208475/12009

--- twsf/email.js
+++ twsf/email.js
@@ -1,195 +1,199 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const logger = require('../logger');
+const logger = require("../logger");
 
-const {addNewGuess, addTwsfError, guessTypes} = require('../database/twsf');
+const { addNewGuess, addTwsfError, guessTypes } = require("../database/twsf");
 
 /*-----------------*\
   PARSING UTILITIES
 \*-----------------*/
 const shiftFromLine = (textLines) => {
-    let fromLine; // = "From: Damonjalis <fake_greek_email@gmail.com>"
+  let fromLine; // = "From: Damonjalis <fake_greek_email@gmail.com>"
 
-    // Shift out lines until we find one starting with "From:"
-    while (!fromLine) {
-        const thisLine = textLines.shift();
-        if (thisLine.startsWith('From:')) {
-            fromLine = thisLine;
-        }
+  // Shift out lines until we find one starting with "From:"
+  while (!fromLine) {
+    const thisLine = textLines.shift();
+    if (thisLine.startsWith("From:")) {
+      fromLine = thisLine;
     }
+  }
 
-    // Parse the line
-    const nickAndEmail = /From: (?<nick>.*?) <(?<email>.*?)>/.exec(
-        fromLine,
-    ).groups;
+  // Parse the line
+  const nickAndEmail = /From: (?<nick>.*?) <(?<email>.*?)>/.exec(
+    fromLine
+  ).groups;
 
-    // Done!
-    return nickAndEmail;
+  // Done!
+  return nickAndEmail;
 };
 const shiftSubjectLine = (textLines) => {
-    let subjectLine; // = "Subject: Thisweeksf"
+  let subjectLine; // = "Subject: Thisweeksf"
 
-    // Shift out lines until we find one starting with "Subject:"
-    while (!subjectLine) {
-        const thisLine = textLines.shift();
-        if (thisLine.startsWith('Subject:')) {
-            subjectLine = thisLine;
-        }
+  // Shift out lines until we find one starting with "Subject:"
+  while (!subjectLine) {
+    const thisLine = textLines.shift();
+    if (thisLine.startsWith("Subject:")) {
+      subjectLine = thisLine;
     }
+  }
 
-    // Parse the line
-    const subject = subjectLine.slice(9);
+  // Parse the line
+  const subject = subjectLine.slice(9);
 
-    // Done!
-    return subject;
+  // Done!
+  return subject;
 };
 const shiftBodyLines = (textLines) => {
-    // "To: info@tom.com\n\n\nYour multiline\nmessage here."
-    let doneLooking = false;
+  // "To: info@tom.com\n\n\nYour multiline\nmessage here."
+  let doneLooking = false;
 
-    // Shift out lines until we find one starting with "To:"
-    while (!doneLooking) {
-        const thisLine = textLines.shift();
-        doneLooking = thisLine.startsWith('To:');
-    }
+  // Shift out lines until we find one starting with "To:"
+  while (!doneLooking) {
+    const thisLine = textLines.shift();
+    doneLooking = thisLine.startsWith("To:");
+  }
 
-    // Shift out whitespace-only lines
-    doneLooking = false;
-    while (!doneLooking) {
-        textLines.shift();
-        doneLooking = /\S/.test(textLines[0]);
-    }
+  // Shift out whitespace-only lines
+  doneLooking = false;
+  while (!doneLooking) {
+    textLines.shift();
+    doneLooking = /\S/.test(textLines[0]);
+  }
 
-    // Capture body lines until we reach block-quoted text
-    let foundBlockquote = false;
-    const bodyLines = [];
-    doneLooking = false;
-    while (!doneLooking) {
-        const thisLine = textLines.shift();
-        foundBlockquote = /^>+ /.test(thisLine);
-        if (!foundBlockquote) bodyLines.push(thisLine);
-        else doneLooking = true;
-    }
+  // Capture body lines until we reach block-quoted text
+  let foundBlockquote = false;
+  const bodyLines = [];
+  doneLooking = false;
+  while (!doneLooking) {
+    const thisLine = textLines.shift();
+    foundBlockquote = /^>+ /.test(thisLine);
+    if (!foundBlockquote) bodyLines.push(thisLine);
+    else doneLooking = true;
+  }
 
-    // If block-quoted text was found, rewind and discard gmail's forward "header."
-    // "On Wed, Aug 3, 2022 at 12:02 PM Name <email> wrote:" (possible line breaks!)
-    let foundHeader = false;
-    let lineToDrop;
-    for (
-        lineToDrop = bodyLines.length;
-        lineToDrop > 0 && !foundHeader;
-        lineToDrop -= !foundHeader // Don't increment past the header
-    ) {
-        const thisLine = bodyLines[lineToDrop];
-        foundHeader = /^On (Sun|Mon|Tue|Wed|Thu|Fri), /.test(thisLine);
-    }
+  // If block-quoted text was found, rewind and discard gmail's forward "header."
+  // "On Wed, Aug 3, 2022 at 12:02 PM Name <email> wrote:" (possible line breaks!)
+  let foundHeader = false;
+  let lineToDrop;
+  for (
+    lineToDrop = bodyLines.length;
+    lineToDrop > 0 && !foundHeader;
+    lineToDrop -= !foundHeader // Don't increment past the header
+  ) {
+    const thisLine = bodyLines[lineToDrop];
+    foundHeader = /^On (Sun|Mon|Tue|Wed|Thu|Fri), /.test(thisLine);
+  }
 
-    // Discard forward "header."
-    if (foundHeader) bodyLines = bodyLines.slice(0, lineToDrop);
+  // Discard forward "header."
+  if (foundHeader) bodyLines = bodyLines.slice(0, lineToDrop);
 
-    // Join the lines we saved.
-    body = bodyLines.join('');
+  // Join the lines we saved.
+  body = bodyLines.join("");
 
-    // Done!
-    return body;
+  // Done!
+  return body;
 };
 
 /*---------------*\
   HANDLER HELPERS
 \*---------------*/
 const parseTextContent = (textContent) => {
-    // Parse forwarded emails. For now, we won't be collecting emails directly
-    // from listeners. Instead, Ben will forward them.
-    // Any of these parsing steps might encounter errors. Let's make future
-    // debugging easier by accumulating error messages as we go and including
-    // them in the database.
-    const parsedElements = {};
-    let parsingErrors = [];
+  // Parse forwarded emails. For now, we won't be collecting emails directly
+  // from listeners. Instead, Ben will forward them.
+  // Any of these parsing steps might encounter errors. Let's make future
+  // debugging easier by accumulating error messages as we go and including
+  // them in the database.
+  const parsedElements = {};
+  let parsingErrors = [];
 
-    // The components we're looking for are separated by line breaks
-    const textLines = textContent.split(/(\r|\n|\r\n)/);
+  // The components we're looking for are separated by line breaks
+  const textLines = textContent.split(/(\r|\n|\r\n)/);
+  logger.info({ textLines });
 
-    // From: Damonjalis <fake_greek_email@gmail.com>
-    try {
-        const {nick, email} = shiftFromLine(textLines);
-        parsedElements.nick = nick;
-        parsedElements.email = email;
-    } catch (error) {
-        parsingErrors.push(['from', error.message]);
-    }
+  // From: Damonjalis <fake_greek_email@gmail.com>
+  try {
+    const { nick, email } = shiftFromLine(textLines);
+    parsedElements.nick = nick;
+    parsedElements.email = email;
+  } catch (error) {
+    parsingErrors.push(["from", error.message]);
+  }
+  logger.info(parsedElements);
 
-    // Subject: Thisweeksf
-    try {
-        // Look for the right line
-        parsedElements.subject = shiftSubjectLine(textLines);
-    } catch (error) {
-        parsingErrors.push(['subject', error.message]);
-    }
+  // Subject: Thisweeksf
+  try {
+    // Look for the right line
+    parsedElements.subject = shiftSubjectLine(textLines);
+  } catch (error) {
+    parsingErrors.push(["subject", error.message]);
+  }
+  logger.info(parsedElements);
 
-    // To: info@tom.com\n\n\nYour multiline\nmessage here.
-    try {
-        // Look for the right line
-        parsedElements.body = shiftBodyLines(textLines);
-    } catch (error) {
-        parsingErrors.push(['body', error.message]);
-    }
+  // To: info@tom.com\n\n\nYour multiline\nmessage here.
+  try {
+    // Look for the right line
+    parsedElements.body = shiftBodyLines(textLines);
+  } catch (error) {
+    parsingErrors.push(["body", error.message]);
+  }
+  logger.info(parsedElements);
 
-    // Nullify parsing errors if appropriate
-    if (parsingErrors.length === 0) parsingErrors = null;
+  // Nullify parsing errors if appropriate
+  if (parsingErrors.length === 0) parsingErrors = null;
 
-    // Done!
-    return {parsedElements, parsingErrors};
+  // Done!
+  return { parsedElements, parsingErrors };
 };
-const guessAndAuthorFromEmail = ({parsedElements, parsingErrors}) => {
-    const subject = parsedElements.subject;
-    const text = parsedElements.body;
-    const guess = {type: guessTypes.EMAIL, subject, text, parsingErrors};
+const guessAndAuthorFromEmail = ({ parsedElements, parsingErrors }) => {
+  const subject = parsedElements.subject;
+  const text = parsedElements.body;
+  const guess = { type: guessTypes.EMAIL, subject, text, parsingErrors };
 
-    const emailAddress = parsedElements.email;
-    const emailName = parsedElements.nick;
-    const callsign = emailName;
-    const author = {emailAddress, emailName, callsign};
+  const emailAddress = parsedElements.email;
+  const emailName = parsedElements.nick;
+  const callsign = emailName;
+  const author = { emailAddress, emailName, callsign };
 
-    return {guess, author};
+  return { guess, author };
 };
 
 /*-------*\
   HANDLER
 \*-------*/
 module.exports = async (textContent) => {
-    try {
-        logger.info("// Parse the email")
-        const {parsedElements, parsingErrors} = parseTextContent(textContent);
-        if (!parsedElements)
-            throw {
-                msg: 'Something went wrong while parsing TWSF email...',
-                parsingErrors,
-            };
+  try {
+    logger.info("// Parse the email");
+    const { parsedElements, parsingErrors } = parseTextContent(textContent);
+    if (!parsedElements)
+      throw {
+        msg: "Something went wrong while parsing TWSF email...",
+        parsingErrors,
+      };
 
-        logger.info("// Prepare database data")
-        const guessAndAuthor = guessAndAuthorFromEmail({
-            parsedElements,
-            parsingErrors,
-        });
-        if (!guessAndAuthor.guess && guessAndAuthor.author)
-            throw {
-                msg: 'Something went wrong while preparing to store TWSF email...',
-                parsingErrors,
-            };
+    logger.info("// Prepare database data");
+    const guessAndAuthor = guessAndAuthorFromEmail({
+      parsedElements,
+      parsingErrors,
+    });
+    if (!guessAndAuthor.guess && guessAndAuthor.author)
+      throw {
+        msg: "Something went wrong while preparing to store TWSF email...",
+        parsingErrors,
+      };
 
-        logger.info("// Store email")
-        const successfullyStored = await addNewGuess(guessAndAuthor);
-        if (!successfullyStored)
-            throw {
-                msg: 'Something went wrong while storing TWSF email...',
-                parsingErrors,
-            };
+    logger.info("// Store email");
+    const successfullyStored = await addNewGuess(guessAndAuthor);
+    if (!successfullyStored)
+      throw {
+        msg: "Something went wrong while storing TWSF email...",
+        parsingErrors,
+      };
 
-        // Done!
-        logger.info('Done storing new TWSF email!');
-    } catch (error) {
-        if (error.msg) logger.error({...error, textContent});
-        else logger.error({error, textContent});
-        addTwsfError(JSON.stringify({error, textContent}));
-    }
+    // Done!
+    logger.info("Done storing new TWSF email!");
+  } catch (error) {
+    if (error.msg) logger.error({ ...error, textContent });
+    else logger.error({ error, textContent });
+    addTwsfError(JSON.stringify({ error, textContent }));
+  }
 };

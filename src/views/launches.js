2f51d208b12fb75e09c767ef31ab26d827f7d736
Ben Etherington <ben.j.etherington@gmail.com>
2022-11-15 08:25:49 -0500

    Add bolding to rocket name

--- src/views/launches.js
+++ src/views/launches.js
@@ -170,9 +170,9 @@ const buildLaunchRow = (launchData) => {
     rocketSection.classList.add('rocket', 'card', 'has-header');
     rowContainer.append(rocketSection);
 
-    const rocketNameEl = document.createElement('p');
+    const rocketNameEl = document.createElement('div');
     rocketNameEl.classList.add('name');
-    rocketNameEl.innerText = rocketName + ' ' + rocketVariant;
+    rocketNameEl.innerHTML = `<b>${rocketName}</b> ${rocketVariant}`;
     rocketSection.append(rocketNameEl);
 
     const companyNameEl = document.createElement('p');

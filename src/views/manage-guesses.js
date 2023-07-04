8901af92ebc0d493ef1940028c76294665efd9c8
Ben Etherington <ben.j.etherington@gmail.com>
2023-07-04 12:53:37 -0400

    Add guess search

--- src/views/manage-guesses.js
+++ src/views/manage-guesses.js
@@ -22,3 +22,31 @@ const loadGuesses = async () => {
     guesses.forEach(addGuessRow);
     setPaginationTotal(count);
 };
+
+/*----------*\
+  SEARCH BOX
+\*----------*/
+const performGuessSearch = (searchString) => {
+    const [yesMatch, notMatch] = getMatchingGuessRows(searchString);
+
+    if (yesMatch.length) {
+        yesMatch.forEach((row) => row.classList.remove('hidden'));
+        notMatch.forEach((row) => row.classList.add('hidden'));
+        document.getElementById('search-button').classList.add('active');
+    } else {
+        notMatch.forEach((row) => row.classList.remove('hidden'));
+        document.getElementById('search-button').classList.remove('active');
+    }
+};
+const getMatchingGuessRows = (searchString) => {
+    const guessRows = [...document.querySelectorAll('.guess-row')];
+    if (searchString.length === 0) return [[], guessRows];
+
+    return guessRows.reduce(
+        ([pass, fail], row) =>
+            row.innerText.toLowerCase().includes(searchString)
+                ? [[...pass, row], fail] // add row to pass list
+                : [pass, [...fail, row]], // add row to fail list
+        [[], []], // start with empty pass/fail arrays
+    );
+};

a36aef784e65474fa71c6d5df44f5a4024f96c02
Glitch (tom-title-suggestion-monitor) <none>
2024-03-31 17:58:56 +0000

    🍧🌛 Checkpoint ./src/routes/email.js:41208475/4069

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,52 +1,59 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const storeNewTwsfEmail = require('../../twsf/email');
+const storeNewTwsfEmail = require("../../twsf/email");
 
 module.exports = (fastify, opts, done) => {
-    fastify.post('/twsf-email', async (request, reply) => {
-        request.log.info('Incoming TWSF email!');
+  fastify.post("/twsf-email", async (request, reply) => {
+    request.log.info("Incoming TWSF email!");
 
-        try {
-            // Grab data from header
-            const authHeader = request.headers.authorization;
-            // Format and encode the correct credentials. This seems easier than
-            // parsing the received header and atob-ing its value. ¯\_(ツ)_/¯
-            const credentials =
-                process.env.EMAIL_ENDPOINT_USERNAME +
-                ':' +
-                process.env.EMAIL_ENDPOINT_PASSWORD;
-            const expectedAuth = `Basic ${btoa(credentials)}`;
-            // Check that the request is valid
-            if (authHeader !== expectedAuth) {
-                request.log.info('Bad auth provided to POST /twsf-email!');
-                return reply.status(401).send(); // 401: not authorized
-            }
+    try {
+      // Grab data from header
+      const authHeader = request.headers.authorization;
+      // Format and encode the correct credentials. This seems easier than
+      // parsing the received header and atob-ing its value. ¯\_(ツ)_/¯
+      const credentials =
+        process.env.EMAIL_ENDPOINT_USERNAME +
+        ":" +
+        process.env.EMAIL_ENDPOINT_PASSWORD;
+      const expectedAuth = `Basic ${btoa(credentials)}`;
+      // Check that the request is valid
+      if (authHeader !== expectedAuth) {
+        request.log.info("Bad auth provided to POST /twsf-email!");
+        return reply.status(401).send(); // 401: not authorized
+      }
 
-            // Grab sender's address from body
-            const from = request.body.envelope.from;
-            // Check that it's from Ben. For now, we won't be collecting emails
-            // directly from listeners. This isn't perfectly secure, but might
-            // save a headache in the future.
-            if (!['zoundspadang@gmail.com', 'ben.etherington@hey.com'].includes(from)) {
-                request.log.info("Email didn't come from Ben");
-                return reply.status(401).send(); // 401: not authorized
-            }
+      // Grab sender's address from body
+      const from = request.body.envelope.from;
+      // Check that it's from Ben. For now, we won't be collecting emails
+      // directly from listeners. This isn't perfectly secure, but might
+      // save a headache in the future.
+      if (
+        ![
+          "zoundspadang@gmail.com",
+          "ben.etherington@hey.com",
+          "dennis@theorbitalmechanics.com",
+          "dwjust@gmail.com",
+        ].includes(from)
+      ) {
+        request.log.info("Email didn't come from Ben");
+        return reply.status(401).send(); // 401: not authorized
+      }
 
-            // Grab plaintext from body
-            const textContent = request.body.plain;
-            // Hand content off for parsing and storage
-            storeNewTwsfEmail(textContent);
+      // Grab plaintext from body
+      const textContent = request.body.plain;
+      // Hand content off for parsing and storage
+      storeNewTwsfEmail(textContent);
 
-            // Return success
-            return reply.send(1);
-        } catch (error) {
-            request.log.error({
-                msg: 'Error encountered while processing TWSF email:',
-                error,
-            });
-            return reply.error(500).send();
-        }
-    });
+      // Return success
+      return reply.send(1);
+    } catch (error) {
+      request.log.error({
+        msg: "Error encountered while processing TWSF email:",
+        error,
+      });
+      return reply.error(500).send();
+    }
+  });
 
-    done();
+  done();
 };
